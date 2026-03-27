28379caaa209d0fd6be298edcb1fb77a44f31a59
GabrielPrindii <hanonymous242@gmail.com>
2026-03-27 19:49:44 +0200

    Initial Commit

---
+++ itechARappUITests/itechARappUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  itechARappUITestsLaunchTests.swift
+//  itechARappUITests
+//
+//  Created by Gabriel Prindii on 3/27/26.
+//
+
+import XCTest
+
+final class itechARappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
